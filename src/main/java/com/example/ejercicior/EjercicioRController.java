5b1b9c82a24d7d428348d3867c524d16842a5fb8
Joelgs13 <050213jgonzalez@gmail.com>
2024-11-10 19:02:09 +0100

    Pantalla dibujada

--- src/main/java/com/example/ejercicior/EjercicioRController.java
+++ src/main/java/com/example/ejercicior/EjercicioRController.java
@@ -4,7 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class HelloController {
+public class EjercicioRController {
 
     @FXML
     private Button btEnviar;
